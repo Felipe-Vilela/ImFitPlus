a74aea990600278ebba52c76471b162ab898b408
Felipe Vilela <felipe.vilela01@outlook.com>
2025-10-25 22:20:34 -0300

    feat: PesoIdealActivity

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/imfitplus/PesoIdealActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/imfitplus/PesoIdealActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc303898x.imfitplus.databinding.ActivityPesoIdealBinding
+import java.text.DecimalFormat
 
 class PesoIdealActivity : AppCompatActivity() {
 
@@ -11,7 +12,7 @@ class PesoIdealActivity : AppCompatActivity() {
         ActivityPesoIdealBinding.inflate(layoutInflater)
     }
 
-    lateinit var dadosPessoais: DadosPessoais
+    private lateinit var dadosPessoais: DadosPessoais
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +27,34 @@ class PesoIdealActivity : AppCompatActivity() {
         }else {
             intent.getParcelableExtra<DadosPessoais>(Constant.EXTRA_PERFIL)!!
         }
+
+        dadosPessoais.let { dados ->
+            val pesoIdeal = calcularPesoIdeal(dados.altura)
+            val diferencaPeso = pesoIdeal - dados.peso
+
+            val df = DecimalFormat("0.00")
+            val pesoIdealFormatado = df.format(pesoIdeal)
+            var diferencaPesoFormatada = df.format(diferencaPeso)
+
+            if (diferencaPeso > 0) {
+                diferencaPesoFormatada = "+$diferencaPesoFormatada"
+            }
+
+            apib.pesoIdealResultadoTv.text = getString(R.string.seu_peso_ideal, pesoIdealFormatado)
+            apib.diferencaPesoResultadoTv.text = getString(R.string.diferenca_peso, diferencaPesoFormatada)
+        }
+
+        apib.voltarBt.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun calcularPesoIdeal(altura: Double): Double {
+        return 22 * (altura * altura)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }

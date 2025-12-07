de5f5cd6beba9237bacf88ba546a7172b3f82444
Felipe Vilela <felipe.vilela01@outlook.com>
2025-12-07 13:20:42 -0300

    fix: fixing adapter

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/imfitplus/adapter/HistoricoAdapter.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/imfitplus/adapter/HistoricoAdapter.kt
@@ -1,47 +1,83 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.imfitplus.ui
 
+import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import br.edu.ifsp.scl.ads.prdm.sc303898x.imfitplus.R
 import br.edu.ifsp.scl.ads.prdm.sc303898x.imfitplus.databinding.TileHistoricoBinding
 import br.edu.ifsp.scl.ads.prdm.sc303898x.imfitplus.model.DadosPessoais
-import java.text.DecimalFormat
 
 class HistoricoAdapter(
-    private val lista: List<DadosPessoais>
-) : RecyclerView.Adapter<HistoricoAdapter.Holder>() {
+    context: Context,
+    private val historicolist: MutableList<DadosPessoais>
+) : ArrayAdapter<DadosPessoais>(
+    context,
+    R.layout.tile_historico,
+    historicolist
+) {
 
-    inner class Holder(val binding: TileHistoricoBinding) :
-        RecyclerView.ViewHolder(binding.root)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val dadosPessoais = historicolist[position]
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding = TileHistoricoBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return Holder(binding)
-    }
+        var historicoTileView = convertView
 
-    override fun getItemCount() = lista.size
+        if (historicoTileView == null){
+            TileHistoricoBinding.inflate(
+                context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater,
+                parent,
+                false
+            ).apply {
+                historicoTileView = root
+                val tileHistoricoViewHolder = TileHistoricoViewHolder(
+                    nomeTv,
+                    idadeTv,
+                    sexoTv,
+                    alturaTv,
+                    pesoTv,
+                    nivelTv,
+                    imcTv,
+                    taxaBasalTv,
+                    gastoTv,
+                    pesoIdealTv,
+                    categoriaTv,
+                    recomendacaoAguaTv)
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        val item = lista[position]
-        val df = DecimalFormat("0.00")
-
-        holder.binding.apply {
-            nomeTv.text = "Nome: ${item.nome}"
-            idadeTv.text = "Idade: ${item.idade}"
-            sexoTv.text = "Sexo: ${item.sexo}"
-            alturaTv.text = "Altura: ${df.format(item.altura)} m"
-            pesoTv.text = "Peso: ${df.format(item.peso)} kg"
-            nivelTv.text = "Nível de atividade: ${item.nivelAtividade}"
-            imcTv.text = "IMC: ${df.format(item.imc)}"
-            taxaBasalTv.text = "Taxa metabólica basal: ${item.tmb}"
-            gastoTv.text = "Gasto calórico: ${item.gastoCalorico}"
-            pesoIdealTv.text = "Peso ideal: ${item.pesoIdeal}"
-            categoriaTv.text = "Categoria: ${item.categoriaImc}"
-            recomendacaoAguaTv.text = "Recomendação de água: ${item.recomendacaoAgua} L"
+                historicoTileView.tag = tileHistoricoViewHolder
+            }
         }
+
+        val tlViewHolder = historicoTileView?.tag as TileHistoricoViewHolder
+        tlViewHolder.nomeTv.text = dadosPessoais.nome
+        tlViewHolder.idadeTv.text = dadosPessoais.idade.toString()
+        tlViewHolder.sexoTv.text = dadosPessoais.sexo
+        tlViewHolder.alturaTv.text = dadosPessoais.altura.toString()
+        tlViewHolder.pesoTv.text = dadosPessoais.peso.toString()
+        tlViewHolder.nivelTv.text = dadosPessoais.nivelAtividade
+        tlViewHolder.imcTv.text = dadosPessoais.imc.toString()
+        tlViewHolder.taxaBasalTv.text = dadosPessoais.tmb.toString()
+        tlViewHolder.gastoTv.text = dadosPessoais.gastoCalorico.toString()
+        tlViewHolder.pesoIdealTv.text = dadosPessoais.pesoIdeal.toString()
+        tlViewHolder.categoriaTv.text = dadosPessoais.categoriaImc
+        tlViewHolder.recomendacaoAguaTv.text = dadosPessoais.recomendacaoAgua.toString()
+
+        return historicoTileView
     }
+    private data class TileHistoricoViewHolder(
+        val nomeTv: TextView,
+        val idadeTv: TextView,
+        val sexoTv: TextView,
+        val alturaTv: TextView,
+        val pesoTv: TextView,
+        val nivelTv: TextView,
+        val imcTv: TextView,
+        val taxaBasalTv: TextView,
+        val gastoTv: TextView,
+        val pesoIdealTv: TextView,
+        val categoriaTv: TextView,
+        val recomendacaoAguaTv: TextView
+    )
 }
